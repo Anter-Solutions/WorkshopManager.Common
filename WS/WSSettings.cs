687ccb28476c801479fb52b816b965399f1de6b2
vladimirmazarakis <87600021+vladimirmazarakis@users.noreply.github.com>
2022-11-18 00:47:50 +0200

    WSSettings and User model added.

---
+++ WS/WSSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopManager.Common.WS
+{
+    public record WSSettings
+    {
+        public WSSettings()
+        {
+            Name = "Default";
+        }
+
+        public string Name { get; set; }
+    }
+}
