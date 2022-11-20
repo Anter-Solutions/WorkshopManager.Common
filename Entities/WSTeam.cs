c369977ff1b0e351870961e5ba326dada3e6b353
vladimirmazarakis <87600021+vladimirmazarakis@users.noreply.github.com>
2022-11-20 19:26:04 +0200

    Team Employee Initial Entities

---
+++ Entities/WSTeam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopManager.Common.Entities
+{
+    public record WSTeam
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ Entities/WSTeamEmployeeRelation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopManager.Common.Entities
+{
+    public record WSTeamEmployeeRelation
+    {
+        public int TeamId { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
